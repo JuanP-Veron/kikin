0dd9115db8cdcdc65468c0baa39e39abe94c0dae
Juan Pablo <juanpabloveronf@gmail.com>
2025-03-26 16:34:22 -0400

    Corrigiendo configuración para deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://JuanP-Veron.github.io/KIKIN-Digital',
+  base:'https://JuanP-Veron.github.io/kikin',
 })
